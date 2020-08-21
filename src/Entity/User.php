9aac166061a952fa1e7915743fd46540bab88eb7
Carl Wilson <carl@prototypecreative.co.uk>
2020-08-21 22:52:51 +0100

    upd: fix invalid entity relation mapping

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -43,7 +43,7 @@ class User implements UserInterface
     private $password;
 
     /**
-     * @ORM\OneToMany(targetEntity=Thread::class, mappedBy="user")
+     * @ORM\OneToMany(targetEntity=Thread::class, mappedBy="owner")
      */
     private $threads;
 
